ef19fbeb5dbdccecc7393c2061d994ac17a924c9
glenn <gweatherson@gmail.com>
2018-12-08 16:26:38 -0800

    refactor js

--- main.js
+++ main.js
@@ -1,35 +1,18 @@
-// Pulling JSON content from https://www.openbrewerydb.org/#documentation
-// @TODO: Clean this up, make it less inline and more functional.
-var getJSON = function(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status, xhr.response);
-      }
-    };
-    xhr.send();
-};
+fetch('https://api.openbrewerydb.org/breweries')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(breweries) {
+    let breweryContainer = document.getElementById("breweries");
+    let brewLength = breweries.length;
+    breweries.forEach(function (brewery, index) {
+      let name    = "<h2>" + brewery['name'] + "</h2>";
+      let website = "<a href=" + brewery['website_url'] + ">" + brewery['website_url'] + "</a>";
+      let city    = brewery["city"];
+      let state   = brewery["state"];
+      let formattedBrewery = '<div>' + name + website + city + ', ' + state + '</div>';
+      breweryContainer.innerHTML += formattedBrewery;
 
-getJSON('https://api.openbrewerydb.org/breweries',
-function(err, data) {
-  if (err !== null) {
-    console.log('Something went wrong: ' + err);
-  } else {
+    });
 
-    var arrayLength = data.length;
-    for (var i = 0; i < arrayLength; i++) {
-        let brewName = data[i]["name"];
-        let brewSite = data[i]["website_url"];
-        let brewCity = data[i]["city"];
-        let brewState = data[i]["state"];
-        document.body.innerHTML += '<h2>' + brewName + '</h2>' + brewCity + ', ' + brewState + '<br /><a href="' + brewSite + '">Visit Website</a>';
-
-    }
-
-  }
-});
+  });
